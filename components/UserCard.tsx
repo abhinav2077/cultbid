d5c76b4accdc52dcb10e3fb95b5213859dbc1c67
Abhinav Jayalwaal <jayalwaalabhinav@gmail.com>
2026-09-07 14:49:30 +0530

    Change Twitter label from 'X / Twitter' to 'X'
    Updated label for Twitter social pill to 'X'.

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -178,7 +178,7 @@ export default function UserCard({
   </SocialPill>
 )}
 {user.socials.twitter && (
-  <SocialPill href={user.socials.twitter} label="X / Twitter">
+  <SocialPill href={user.socials.twitter} label="X">
     <Image src={xIcon} alt="X" width={14} height={14} unoptimized />
   </SocialPill>
 )}
