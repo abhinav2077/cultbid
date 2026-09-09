0d5c0d43c0b67b508d1c0501865186ed714ed4db
Abhinav Jayalwaal <jayalwaalabhinav@gmail.com>
2026-09-09 12:53:39 +0530

    Increase gap between user cards in Leaderboard

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -37,7 +37,7 @@ export default function Leaderboard({
         </div>
       ) : (
         <LayoutGroup>
-          <motion.ul layout className="flex flex-col gap-2">
+          <motion.ul layout className="flex flex-col gap-5">
             {users.map((user, i) => (
               <UserCard
                 key={user.id}
